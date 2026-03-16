f8a943b02659a31818ad501f992c79f727beffdd
kingemm <kinge7916@gmail.com>
2026-03-16 16:20:41 +0100

    Week 02 Done

--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -7,37 +7,37 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 public class PriorityQueueTests
 {
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
+    // Scenario: Test for PriorityItem with the most priority
+    // Expected Result: Emmanuel
+    // Defect(s) Found: Can't iterate through the lists completely when finding the highest Priority
     public void TestPriorityQueue_1()
     {
-        // Test for PriorityItem with the most priority
+        // 
 
         var priorityQueue = new PriorityQueue();
-        priorityQueue.Enqueue("Emmanuel", 8);
+        priorityQueue.Enqueue("Emmanuel", 10);
         priorityQueue.Enqueue("Abel", 7);
         priorityQueue.Enqueue("Favour", 5);
         priorityQueue.Enqueue("Angel", 10);
         Debug.WriteLine(priorityQueue.ToString());
         // Assert.Fail("Implement the test case and then remove this.");
-        Assert.AreEqual(priorityQueue.Dequeue(), "Angel");
+        Assert.AreEqual(priorityQueue.Dequeue(), "Emmanuel");
     }
 
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
+    // Scenario: Iterate through the priorityQueue and ensure they follow proper order when dequeued
+        // Tests if the Dequeue method in the PriorityQueue follows proper order (priority) to remove elements
+    // Expected Result: "Engine", "Gearbox", "Propeller Shaft", "Differential", "Rear Axles"
+    // Defect(s) Found: Items don't get removed after been dequeued
     public void TestPriorityQueue_2()
     {
-        // Tests if the Dequeue method in the PriorityQueue follows proper order (priority) to remove elements
 
         var priorityQueue = new PriorityQueue();
         priorityQueue.Enqueue("Differential", 7);
         priorityQueue.Enqueue("Engine", 10);
         priorityQueue.Enqueue("Rear Axles", 6);
         priorityQueue.Enqueue("Body", 4);
-        priorityQueue.Enqueue("Gearbox", 9);
+        priorityQueue.Enqueue("Gearbox", 10);
         priorityQueue.Enqueue("Wheel", 5);
         priorityQueue.Enqueue("Propeller Shaft", 8);
         // priorityQueue.Enqueue("Engine", 10);
@@ -53,12 +53,12 @@ public class PriorityQueueTests
     }
 
      [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
+    // Scenario: Has an error message ("The queue is empty.") when Queue is empty
+    // Test PriorityQueue for handling error in the case of where no items are in the list
+    // Expected Result: "The queue is empty."
+    // Defect(s) Found: No actual deffect found just want to check
     public void TestPriorityQueue_3()
     {
-        // Test PriorityQueue for handling error in the case of where no items are in the list
         var priorityQueue = new PriorityQueue();
         try
         {
